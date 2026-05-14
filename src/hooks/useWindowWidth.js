c76a58cf49881ef6de274bbf5a98177f9775f642
agustin-mondati <agustinmondati@gmail.com>
2026-05-14 13:49:06 -0300

    fix panel resultados de 5 a 4 cards

--- src/hooks/useWindowWidth.js
+++ src/hooks/useWindowWidth.js
@@ -11,7 +11,7 @@ export function useWindowWidth() {
 
   if (width >= 1920) return { sidePadding: '400px', cardFlex: 'calc(20% - 13px)' }
   if (width >= 1536) return { sidePadding: '200px', cardFlex: 'calc(20% - 13px)' }
-  if (width >= 1280) return { sidePadding: '160px', cardFlex: 'calc(20% - 13px)' }
-  if (width >= 1024) return { sidePadding: '80px',  cardFlex: 'calc(20% - 13px)' }
+  if (width >= 1280) return { sidePadding: '160px', cardFlex: 'calc(25% - 12px)' }
+  if (width >= 1024) return { sidePadding: '80px',  cardFlex: 'calc(25% - 12px)' }
   return                   { sidePadding: '40px',  cardFlex: 'calc(33.333% - 11px)' }
 }
